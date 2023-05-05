0fb152c4fdeec27c102568c78ae02ab1a3b27c26
Thompson <brianna1@live.missouristate.edu>
2023-05-05 14:27:25 -0500

    Ch10 CriminalIntent Commit 2

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListViewModel.kt
@@ -0,0 +1,24 @@
+package com.bignerdranch.android.criminalintent
+
+import androidx.lifecycle.ViewModel
+import java.util.*
+
+// 10.2 Create a CrimeListViewModel class that extends (inherits from) ViewModel
+class CrimeListViewModel : ViewModel() {
+
+    // Add a property to store a list of Crimes
+    val crimes = mutableListOf<Crime>()
+
+    // In the following init block, populate the list with dummy data
+    init {
+        for (i in 0 until 100) { // this will populate the list with 100 Crime objects
+            val crime = Crime(
+                id = UUID.randomUUID(),
+                title ="Crime #$i",
+                date = Date(),
+                isSolved = i % 2 == 0
+            )
+            crimes += crime
+        }
+    }
+}
